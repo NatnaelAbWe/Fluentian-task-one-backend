0d032d8e8a77ac4e23f034f48e2cec32b7fad4ae
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-04-06 23:59:03 +0300

    fix: package version

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,9 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from .database import SessionLocal
+from .models import User
 
 SECRET_KEY = "secret"
 ALGORITHM = "HS256"
@@ -10,3 +14,18 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    db = SessionLocal()
+    try:
+        payload = jwt.decode(token, "secret", algorithms=["HS256"])
+        user = db.query(User).filter(User.id == payload.get("user_id")).first()
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
